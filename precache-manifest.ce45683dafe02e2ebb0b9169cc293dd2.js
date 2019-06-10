9323c43084e022c6011c647db81ab634c7c21ea0
Robert H <robertjameshardin@aol.com>
2019-06-10 16:27:59 -0500

    Updates

---
+++ precache-manifest.ce45683dafe02e2ebb0b9169cc293dd2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b238dfb07161ad9e04ef563c06a451b",
+    "url": "/ClickEmDead/index.html"
+  },
+  {
+    "revision": "054e5193bfd552b372e6",
+    "url": "/ClickEmDead/static/css/main.7bff9496.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/ClickEmDead/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "054e5193bfd552b372e6",
+    "url": "/ClickEmDead/static/js/main.fec4d978.chunk.js"
+  },
+  {
+    "revision": "be54faf027c975b5bb48",
+    "url": "/ClickEmDead/static/js/runtime~main.8552411f.js"
+  }
+]);
